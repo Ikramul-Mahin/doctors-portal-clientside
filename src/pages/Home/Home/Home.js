826cddf67027ceb036cda304b02b3ba910dcbda4
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2023-09-21 23:30:35 +0600

    card section done

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Infocard from '../Infocard/Infocard';
+
 
 const Home = () => {
     return (
         <div className='mx-5'>
             <Banner></Banner>
+            <Infocard></Infocard>
         </div>
     );
 };
