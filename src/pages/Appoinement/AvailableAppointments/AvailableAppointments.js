3d183528010459f7780990bdf705885324e92155
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2023-09-30 12:39:33 +0600

    modal added

--- src/pages/Appoinement/AvailableAppointments/AvailableAppointments.js
+++ src/pages/Appoinement/AvailableAppointments/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import AppointmentOption from './AppointmentOption';
+import BookingModal from '../BookingModal/BookingModal';
 
 const AvailableAppointments = ({ selectedDate }) => {
     const [appointmentOptions, setAppointmentoptions] = useState([])
+    const [treatment, setTreatment] = useState(null)
 
     useEffect(() => {
         fetch('appointmentOptions.json')
@@ -19,9 +21,16 @@ const AvailableAppointments = ({ selectedDate }) => {
                     appointmentOptions.map(option => <AppointmentOption
                         key={option._id}
                         appointmentOption={option}
+                        setTreatment={setTreatment}
                     ></AppointmentOption>)
                 }
             </div>
+            {
+                treatment &&
+                <BookingModal
+                    treatment={treatment}
+                ></BookingModal>
+            }
         </section>
     );
 };
